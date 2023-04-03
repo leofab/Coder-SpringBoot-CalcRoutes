cb62f5c73825d5a7b739d31406735dc03efa3559
leofab <leofabr3@gmail.com>
2023-04-03 15:32:54 -0300

    index.html form method && action implement

--- src/main/java/com/coder/desafiospringbootcalccontroller/Controller/CalcController.java
+++ src/main/java/com/coder/desafiospringbootcalccontroller/Controller/CalcController.java
@@ -1,12 +1,14 @@
 package com.coder.desafiospringbootcalccontroller.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculadora")
 public class CalcController {
 
-    @GetMapping(path = "/calculadora")
+    @GetMapping
     public String CalculadoraIndex(){
         return "Working";
     }
