fa0da965763189a6bd0db7ded96533dd0c6ca7f5
leofab <leofabr3@gmail.com>
2023-04-03 16:34:19 -0300

    CalcController snd try get element from html to make method failure

--- src/main/java/com/coder/desafiospringbootcalccontroller/Controller/CalcController.java
+++ src/main/java/com/coder/desafiospringbootcalccontroller/Controller/CalcController.java
@@ -1,15 +1,16 @@
 package com.coder.desafiospringbootcalccontroller.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/calculadora")
 public class CalcController {
 
-    @GetMapping
-    public String CalculadoraIndex(){
-        return "Working";
+    @GetMapping("{firstValue}")
+    public Integer CalculadoraIndex(@PathVariable(name = "firstValue") Model model, Integer firstValue){
+        System.out.println(firstValue);
+        return firstValue;
     }
+
 }
