39eefc5ea4919093041a369ce843372237389c0d
leofab <leofabr3@gmail.com>
2023-04-04 10:11:06 -0300

    ProductController DeleteMapping annotation and method implement

--- src/main/java/com/coder/desafiospringbootcalccontroller/Controller/ProdutController.java
+++ src/main/java/com/coder/desafiospringbootcalccontroller/Controller/ProdutController.java
@@ -6,8 +6,8 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -16,11 +16,23 @@ public class ProdutController {
     @Autowired
     ProductRepository productRepository;
 
+
+//    @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+//    public @ResponseBody Product insertProduct(@Valid @RequestParam String name){
+//        Product product = new Product(name);
+//        productRepository.save(product);
+//        return product;
+//    }
+
     @GetMapping
     public List<Product> findAll(){
         List<Product> products = productRepository.findAll();
         System.out.println(products);
         return products;
+    }
+    @GetMapping(path = "/{id}")
+    public Optional<Product> findById(@PathVariable Long id){
+        return productRepository.findById(id);
     }
 
     @PostMapping
@@ -31,12 +43,23 @@ public class ProdutController {
     }
 
     @PutMapping("/{id}")
-    public @ResponseBody Product updateProduct(@Valid @RequestParam(name = "id") Long id, String name, double price, double disc){
+    public @ResponseBody Product updateProduct(
+            @Valid
+            @RequestParam(name = "id") Long id,
+            String name,
+            double price,
+            double disc)
+    {
         Product productUpdate = productRepository.getReferenceById(id);
         productUpdate.setName(name);
         productUpdate.setPrice(price);
         productUpdate.setDisc(disc);
         return productRepository.save(productUpdate);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deleteProductById(@PathVariable Long id){
+        productRepository.deleteById(id);
     }
 
 }
